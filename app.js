f5a7da7b94a9195aa179e5fb3cbecf358343dd8f
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-05-01 11:46:24 +0530

    files

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
+const Place = require("./models/place.js")
+
 
 
 main().then(()=>{
@@ -11,8 +13,21 @@ app.get("/",(req,res)=>{
     res.send("Hello , Mathew")
 })
 
+app.get("/demoPlace",async(req,res)=>{
+    let samplePlace = new Place({
+        title:"My New Hotel",
+        description:"India is very beautiful",
+        price:6600,
+        location:"Vrindavan",
+        country:"India" 
+    })
+    await samplePlace.save()
+    console.log("Sample was saved");
+    res.send("sucessfull testing")
+})
+
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+  await mongoose.connect('mongodb://127.0.0.1:27017/Hotel');
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
